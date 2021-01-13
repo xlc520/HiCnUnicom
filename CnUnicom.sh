c0222297bd589d7b3e4f1188c6cbde55db5a6e87
xlc520 <56480807+xlc520@users.noreply.github.com>
2021-01-13 10:31:53 +0800

    修改UA

--- CnUnicom.sh
+++ CnUnicom.sh
@@ -19,7 +19,7 @@ unicom_version=8.0100
 deviceId=$(shuf -i 123456789012345-987654321012345 -n 1)
 
 # 安卓手机端APP登录过的使用这个UA
-UA="Mozilla/5.0 (Linux; Android 6.0.1; oneplus a5010 Build/V417IR; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36; unicom{version:android@$unicom_version,desmobile:$username};devicetype{deviceBrand:Oneplus,deviceModel:oneplus a5010}"
+UA="Mozilla/5.0 (Linux; Android 10; Redmi K30 5G Build/QKQ1.191222.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.101 Mobile Safari/537.36; unicom{version:android@8.0100,desmobile:$username};devicetype{deviceBrand:Xiaomi,deviceModel:Redmi K30 5G};{yw_code:}"
 
 # 苹果手机端APP登录过的使用这个UA
 #UA="ChinaUnicom4.x/176 CFNetwork/1121.2.2 Darwin/19.2.0"
